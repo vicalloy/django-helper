7f576e76d3f25cc9696caf4fc1f421fff7f16cd4
vicalloy <zbirder@gmail.com>
2010-11-29 15:13:18 +0800

    + setup.py

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+version = '0.8'
+
+LONG_DESCRIPTION = """
+"""
+
+setup(
+    name='django-helper',
+    version=version,
+    description="A set of useful function/tags/filter for Django.",
+    long_description=LONG_DESCRIPTION,
+    classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Framework :: Django",
+        "Environment :: Web Environment",
+    ],
+    keywords='helper,django',
+    author='vicalloy',
+    author_email='zbirder@gmail.com',
+    url='https://github.com/vicalloy/django-helper/',
+    license='BSD',
+    packages=find_packages(),
+    package_data = {
+        'djangohelper': [
+        ],
+    },
+    include_package_data=True,
+    zip_safe=False,
+)
